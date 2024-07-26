87aa4abca1c931d762c619fc0aac52e11f1c90c4
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-26 20:43:44 +0530

    Add project files.

---
+++ OverloadProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadProject
+{
+    internal class Program
+    {
+        public void Test()
+        {
+            Console.WriteLine("1ST METHOD");
+
+        }
+        public void Test(int i)
+        {
+            Console.WriteLine("2nd METHOD");
+
+        }
+        public void Test(String s)
+        {
+            Console.WriteLine("3rd METHOD");
+
+        }
+        public void Test(int i , string s)
+        {
+            Console.WriteLine("4th METHOD");
+
+        }
+      
+        public void Test(String s , int i)
+        {
+            Console.WriteLine("5th METHOD");
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            p.Test(5);
+            p.Test("Abhishek");
+            p.Test(69,"abc");
+            p.Test("abc",69);
+
+               Console.ReadLine();
+        }
+    }
+}
